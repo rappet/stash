ee7ab0afc32f9309a019ca562d2dba93d6fa913c
Raphael Peters <rappet@rappet.de>
2023-04-24 17:10:32 +0200

    fs/misc/aead-bench: Add flake

---
+++ projects/fs/misc/aead-bench/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Ultrasonic data transfer rust development environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          apple_pkgs =
+            if pkgs.stdenv.isDarwin then with pkgs.darwin.apple_sdk.frameworks; [
+              Cocoa
+              MetalKit
+            ] else [ ];
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              libiconv
+            ] ++ apple_pkgs;
+          };
+        }
+      );
+}
