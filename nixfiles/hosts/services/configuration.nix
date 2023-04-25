92f2bddac068a93a68d1396bc0c48d5074ffb04f
Raphael Peters <rappet@rappet.de>
2023-04-25 20:25:27 +0200

    nixos: Update networking, SSH and boot for services host

--- nixfiles/hosts/services/configuration.nix
+++ nixfiles/hosts/services/configuration.nix
@@ -4,15 +4,21 @@
     ../../common.nix
   ];
 
-  boot.cleanTmpDir = true;
+  boot.tmp.cleanOnBoot = true;
   zramSwap.enable = true;
   networking.hostName = "services";
   networking.domain = "rappet.xyz";
   services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMz+WvOHfl9Er2QIdQsP/z4Qifk8uj75RfNpVa2WVDr rappet@MacBook-Air-von-Raphael.local" 
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO5JIcEqbIaq/pBCbaU1AV3V2Mor1ZdgKoO3O9vJqW4f rappet@katze" 
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDZGkHoe235ue1fUvy8XjGStLLwTcFllFJ3hStKj9ahT rappet@x230.rappet.de" 
-  ];
   security.sudo.wheelNeedsPassword = false;
+
+  networking = {
+    interfaces.enp1s0.ipv6.addresses = [{
+      address = "2a01:4f8:c012:b412::1";
+      prefixLength = 64;
+    }];
+    defaultGateway6 = {
+      address = "fe80::1";
+      interface = "enp1s0";
+    };
+  };
 }
