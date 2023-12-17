6a2c9e1acb5193ef08a6ea7227c473454a07ac74
Raphael Peters <rappet@rappet.de>
2023-12-17 15:05:07 +0100

    nix/services: Enable nat

--- nixfiles/hosts/services/configuration.nix
+++ nixfiles/hosts/services/configuration.nix
@@ -30,13 +30,19 @@
   networking = {
     interfaces.enp1s0.ipv6.addresses = [{
       address = "2a01:4f8:c012:b412::1";
-      prefixLength = 64;
+      prefixLength = 128;
     }];
     defaultGateway6 = {
       address = "fe80::1";
       interface = "enp1s0";
     };
     extraHosts = "167.235.255.49 ns1.rappet.xyz";
+    nat = {
+      enable = true;
+      internalInterfaces = [ "ve-+" ];
+      externalInterfaces = "enp1s0";
+      enableIPv6 = true;
+    };
   };
 
   users.users.apple-upload = {
