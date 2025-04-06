9111b70eb41ff94fef7df232b2f83b39a16b53fd
Raphael Peters <rappet@rappet.de>
2025-04-06 12:12:19 +0200

    nix: Clearup jellyfin config

--- nixfiles/services/apps/jellyfin.nix
+++ nixfiles/services/apps/jellyfin.nix
@@ -27,6 +27,7 @@ rec {
       services.jellyfin = {
         enable = true;
         openFirewall = true;
+        #proxyPass = "http://127.0.0.1:8096";
       };
 
       system.stateVersion = "23.05";
@@ -45,46 +46,12 @@ rec {
     };
   };
 
-  virtualisation.podman = {
-    enable = true;
-    dockerSocket.enable = true;
-    dockerCompat = true;
-    autoPrune = {
-      enable = true;
-      dates = "daily";
-      flags = [ "--all" ];
-    };
-    #  defaultNetwork.settings.dns_enabled = true;
-  };
-
-  networking.firewall = {
-    trustedInterfaces = [ "podman0" ];
-    interfaces."podman+".allowedUDPPorts = [ 53 ];
-    interfaces."podman+".allowedTCPPorts = [ 53 ];
-    allowedUDPPorts = [ 53 ];
-  };
-
-  #services.resolved.enable = true;
-
-  #virtualisation.oci-containers.containers.jellyfin = {
-  #  image = "docker.io/jellyfin/jellyfin:latest";
-  #  autoStart = true;
-  #  ports = ["127.0.0.1:8096:8096"];
-  #  volumes = [
-  #    "jellyfin-config:/config"
-  #    "jellyfin-cache:/cache"
-  #    "/var/media:/media"
-  #  ];
-  #  extraOptions = [ "--network=host" ];
-  #};
-
   services.nginx.virtualHosts.${domain} = {
     forceSSL = true;
     sslCertificate = "/var/lib/acme/rappet.xyz/fullchain.pem";
     sslCertificateKey = "/var/lib/acme/rappet.xyz/key.pem";
     locations."/" = {
       proxyPass = "http://${config.containers.jellyfin.localAddress}:8096";
-      #proxyPass = "http://127.0.0.1:8096";
     };
   };
 }
