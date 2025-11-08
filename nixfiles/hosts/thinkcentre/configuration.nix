862758e453c7dc135bb72815b60786ab735e5423
Raphael Peters <rappet@rappet.de>
2025-11-08 10:05:39 +0100

    Âthinkcentre: Add libvirt

--- nixfiles/hosts/thinkcentre/configuration.nix
+++ nixfiles/hosts/thinkcentre/configuration.nix
@@ -111,4 +111,18 @@
       User = "root";
     };
   };
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+    };
+  };
+
+  users.users.rappet = {
+    extraGroups = [ "libvirtd" ];
+  };
+
 }
